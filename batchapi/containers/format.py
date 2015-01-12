5abe5b237e3ef7eddd1af181d60a92b86fdb0146
Roy Van Liew <vanliewroy@gmail.com>
2015-01-12 17:40:13 -0500

    Modified functionality of forum post script so more than one set can be appended to the post, and the outline structure at the top is different

--- batchapi/containers/format.py
+++ batchapi/containers/format.py
@@ -25,6 +25,9 @@ formatNotesLogger.addHandler(formatNotesFileH)  # File Handler add
 formatNotesLogger.addHandler(formatNotesConsoleH)  # Console Handler add
 
 def getSetNumber(setName):
+    """
+    Remember this returns a string, not an integer
+    """
     formatNotesLogger.info("getSetNumber: Retrieving Set Number")
     try:
         setNumSearch = re.search(".*([\d]+)$", setName)
@@ -60,15 +63,15 @@ class FormatNotes():
         Constructor
         """
         self.path = notesDir
-        self.setName = str(os.path.basename(os.path.normpath(self.path)).strip())
-        self.setNumber = getSetNumber(self.setName)
-        self.setPostFile = "post_set" + self.setNumber + ".txt"
-        self.notesFiles = os.listdir(self.path)
+        self.setDirs = os.listdir(self.path) # Gives a list of the set folders containing notes
+        self.setPostFile = "forum_post.txt"
+        self.setNums = [] # List storing numbers of the sets as strings, not integers.
+        self.setInfo = {} # Dictionary storing set numbers and judge lists for each set
 
     def getJudgeName(self, notesFileName):
         """
         Parses judge name from input judge notes file.
-        e.g DossarLX ODI from DossarLX ODI_FormatTest.txt
+        e.g DossarLX ODI from DossarLX ODI_Format.txt
         File must be in format of <JudgeName>_Format -- anything else can come after
         """
         formatNotesLogger.info("getJudgeName: Retrieving Judge Name from Notes File '%s'", notesFileName)
@@ -85,40 +88,96 @@ class FormatNotes():
             formatNotesLogger.warning("getJudgeName: {0}: {1}".format(sys.exc_info()[0].__name__,
                                                                       str(sys.exc_info()[1])))
 
+    def getSetJudgeInfo(self):
+        """
+        Goes through the set folders, parses set number from folder name,
+        and then gets the judge name for each notes text file in those folders.
+        So basically for each set directory get set number and judges for those sets.
+        Set folders should be in the form of set<number>, so e.g. set1 set2
+        """
+        formatNotesLogger.info("getSetJudgeInfo: Getting Set and Judge Info")
+        try:
+            for setDir in self.setDirs:
+
+                # Retrieve set number from the set folder directory.
+                setNumber = getSetNumber(setDir) # Retrieve set number from folder name
+                self.setNums.append(setNumber) # Add the set number to the list of set numbers.
+
+                # Change working directory to this set folder.
+                fullSetDir = os.path.join(self.path, setDir)
+                os.chdir(fullSetDir) # Go into the set folder
+
+                # List all the judge notes file in the set directory, then get judges from text files.
+                notesFiles = os.listdir(fullSetDir)
+                setJudgeList = []
+                for notesFile in notesFiles:
+                    judge = self.getJudgeName(notesFile)
+                    setJudgeList.append(judge) # Add judge to the set's judge list
+                self.setInfo[setNumber] = setJudgeList # Make the set number entry to this judge list
+        except:
+            formatNotesLogger.warning("getSetJudgeInfo: {0}: {1}".format(sys.exc_info()[0].__name__,
+                                                                         str(sys.exc_info()[1])))
+
     def makeFormattedPost(self):
-        formatNotesLogger.info("printFormattedPost: Attempting to create formatted post.")
+        """
+        Puts the outline at the top of the post of the set numbers and judges, then
+        appends the judge notes for each set. The Batch (e.g. July/August) will be put
+        manually after the .txt file from this script is generated to prevent convoluted code.
+        """
+        formatNotesLogger.info("makeFormattedPost: Attempting to create formatted post.")
         try:
             os.chdir(self.path)
             with open(self.setPostFile, 'w') as post:
 
-                # Write the top of the post. This also includes the order of the judges.
-                post.write("[b][size=7]SET {0}[/size][/b]\n".format(self.setNumber))
-                counter = 1 # For order of the judges
-                for notesFile in self.notesFiles:
-                    judge = self.getJudgeName(notesFile)
-                    post.write("\n[b]{0}.) {1}[/b]".format(counter,judge))
-                    counter += 1
-                post.write("\n")
+                # Write the top outline of the post. This also includes the order of the judges.
+                alphabet = "abcdefghijklmnopqrstuvwxyz"
+                setCounter = 1 # Sets will be listed with numbers
+                for setNum in self.setNums:
+                    judgeCounter = 0 # Judges will be listed with alphabet indices
+                    judgesInSet = self.setInfo[setNum]
+                    post.write("[b][size=4]{0}.) SET {1}[/size][/b]\n".format(setCounter,setNum))
+                    for judge in judgesInSet:
+                        letterForList = alphabet[judgeCounter]
+                        post.write("[b]{0}.) {1}[/b]\n".format(letterForList,judge))
+                        judgeCounter += 1
+                    setCounter += 1
+                post.write("\n") # This deals with spacing in the first Set posted.
 
-                # Write out notes for each judge in the set.
-                for notesFile in self.notesFiles:
-                    judge = self.getJudgeName(notesFile)
-                    post.write("\n[b][size=4]=== JUDGE: {0} ===[/size][/b]\n".format(judge))
-                    with open(notesFile) as judgeFile:
-                        for line in judgeFile:
-                            line = line.strip()
-                            songLine = re.search("^\[.*\].*\{.*\}[\s]*\(.*\)$", line)
-                            if songLine is not None:
-                                lineToFormat = songLine.group(0)
-                                formattedLine = "[b]" + lineToFormat + "[/b]" 
-                                post.write("\n"+formattedLine)
-                            else:
-                                post.write("\n"+line)
-                        post.write("\n\n")
-                    judgeFile.close()
+                # Now time to write the sets and the notes for them.
+                setDirCounter = 0 # For use in determining set directory to change to.
+                for setNum in self.setNums:
+
+                    # Change working directory to this set folder.
+                    fullSetDir = os.path.join(self.path, self.setDirs[setDirCounter])
+                    os.chdir(fullSetDir) # Go into the set folder
+                    notesFiles = os.listdir(fullSetDir) # List all judge notes files in set.
+
+                    # Write judge notes info
+                    post.write("[b][size=7]SET {0}[/size][/b]".format(setNum))
+                    judgeList = self.setInfo[setNum] # Retrieve judge list for the set
+                    judgeCounter = 0 # For use in determining judge notes file to use
+                    for judge in judgeList:
+                        post.write("\n\n[b][size=4]=== JUDGE: {0} ===[/size][/b]\n".format(judge))
+                        fileToOpen = notesFiles[judgeCounter]
+                        with open(fileToOpen) as judgeFile:
+                            for line in judgeFile:
+                                line = line.strip()
+                                songLine = re.search("^\[.*\].*\{.*\}[\s]*\(.*\)$", line)
+                                if songLine is not None:
+                                    lineToFormat = songLine.group(0)
+                                    formattedLine = "[b]" + lineToFormat + "[/b]" 
+                                    post.write("\n"+formattedLine)
+                                else:
+                                    post.write("\n"+line)
+                        judgeFile.close()
+                        judgeCounter += 1
+                    setDirCounter += 1
+                        
             post.close()
             formatNotesLogger.info("printFormattedPost: Successfully wrote '%s'", self.setPostFile)
         except:
             formatNotesLogger.warning("makeFormattedPost: {0}: {1}".format(sys.exc_info()[0].__name__,
                                                                            str(sys.exc_info()[1])))
+            formatNotesLogger.warning("Are you sure the directory only has the set folders with "
+                                      "the notes files?")
 
--- batchapi/forumpost.py
+++ batchapi/forumpost.py
@@ -6,8 +6,11 @@ from containers.format import *
 if __name__ == "__main__":
 
     print(">>> format.py takes a set of judge notes and combines them into a forum post.")
-    notesPath = (input(">>> Input full path to notes directory for set: ")).strip()
+    notesPath = (input(">>> Input full path to directory with sets folders for judge notes: ")).strip()
     notesFormat = FormatNotes(notesPath)
+
+    # Get the judges and all set info.
+    notesFormat.getSetJudgeInfo()
 
     # Print out formatting.
     print(">>> Creating formatted post of the batch set.")
